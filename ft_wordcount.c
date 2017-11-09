1b48c1249c5ea381a966195de9e3d99df602bac9
Mitchel Osborne <osborne.mitchel@gmail.com>
2017-11-09 15:45:52 -0800

    Add files via upload

--- ft_wordcount.c
+++ ft_wordcount.c
@@ -1,24 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iswhitespace.c                                  :+:      :+:    :+:   */
+/*   ft_wordcount.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mosborne <mosborne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/10 15:24:15 by mosborne          #+#    #+#             */
-/*   Updated: 2017/10/18 11:28:42 by mosborne         ###   ########.fr       */
+/*   Created: 2017/10/16 12:41:53 by mosborne          #+#    #+#             */
+/*   Updated: 2017/10/18 11:30:28 by mosborne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_iswhitespace(char *str)
+int	ft_wordcount(char *str, char c)
 {
-	while (*str)
+	int i;
+	int x;
+	int y;
+
+	i = 0;
+	y = 0;
+	x = 0;
+	while (str[i])
 	{
-		if ((*str == ' ') || (*str == '\t') || (*str == '\n') ||
-			(*str == '\v') || (*str == '\f') || (*str == '\r'))
-			return (1);
+		if (str[i] != c)
+			if (x == 0)
+				y++;
+			else
+				x = 1;
+		else
+			x = 0;
+		i++;
 	}
-	return (0);
+	return (y);
 }
